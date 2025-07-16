502f078ba9b25d95740fd2d7ada9618584dad16a
Kayne Rodrigo <kayneuriel.rodrigo@gcash.com>
2025-07-16 13:58:13 +0800

    added changes

---
+++ session1/challenge-lab2/myfirstapi/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.example.demo.myfirstapi.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable; // Import this!
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HelloWorldController {
+
+    @Value("${app.greeting.message}")
+    private String customGreeting;
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return customGreeting;
+    }
+
+    @GetMapping("/greeting")
+    public String greetWithParam(
+            @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
+    }
+
+
+}
